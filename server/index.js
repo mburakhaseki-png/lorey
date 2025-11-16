b7be4fa180e0e20e9c9b0592bd748e6441ba0046
Burak Haseki <mburakhaseki@gmail.com>
2025-11-16 20:59:06 +0300

    Revert handler format - use Express app directly for Vercel

--- server/index.js
+++ server/index.js
@@ -77,9 +77,8 @@ app.use((err, req, res, next) => {
 });
 
 // Export for Vercel serverless functions
-// Vercel expects the handler to be exported as default
+// Vercel expects the Express app to be exported directly
 module.exports = app;
-module.exports.default = app;
 
 // Only listen if running locally (not in Vercel)
 if (process.env.VERCEL !== '1') {
